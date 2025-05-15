aabc8749ce77cee9557fb1c56a8653c6201b234a
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-05-15 17:20:32 +0200

    message

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main (String[] args){
+        String colour = "Purple";
+        int shades = 31;
+        double numberOfColour = 9.89;
+        char decorativeSymbol = '@';
+        boolean isOrangeTheBest = false;
+        String meaning = "null";
+        System.out.println(colour);
+        System.out.println(shades);
+        System.out.println(numberOfColour);
+        System.out.println(decorativeSymbol);
+        System.out.println(isOrangeTheBest);
+        System.out.println(meaning);
+    }
+}
+
---
+++ kodilla-intro/src/main/java/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        String myName = "Liudmyla";
+        int myAge = 24;
+        double height = 1.61;
+        char carClass = 'B';
+        boolean isStartingTesting = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isStartingTesting);
+    }
+}
