f7d5a3f9e2575339a41428e4489c52716c5d8d33
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-06-27 22:27:21 +0200

    Checking if a year is a leap year.

--- kodilla-intro/src/main/java/com/kodilla/LeapYear2.java
+++ kodilla-intro/src/main/java/com/kodilla/LeapYear2.java
@@ -1,14 +1,25 @@
-public class LeapYear2 {
+public class LeapYear2{
     public static void main(String[] args) {
         int year = 2024;
+        boolean leapYear = (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
 
-        boolean LeapYear = (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
-
-        System.out.println(year);
-        if (LeapYear == true) {
+        System.out.println("Year: " + year);
+        if (leapYear) {
             System.out.println("Leap year");
         } else {
-            System.out.println("It isn`t a leap year");
+            System.out.println("It isn’t a leap year");
         }
+
+        int startYear = 1582;
+        int firstLeapYear = 0;
+
+        for (int currentYear = startYear; currentYear <= 2025; currentYear++) {
+            if ((currentYear % 4 == 0 && currentYear % 100 != 0) || (currentYear % 400 == 0)) {
+                firstLeapYear = currentYear;
+                break;
+            }
+        }
+
+        System.out.println("The first leap year is: " + firstLeapYear);
     }
 }
