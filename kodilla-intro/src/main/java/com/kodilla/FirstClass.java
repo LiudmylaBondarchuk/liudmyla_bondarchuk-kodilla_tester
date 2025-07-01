47b1c94d75933bd42060df264598159e58608d44
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-07-01 23:44:46 +0200

    Simplify code and adhere to DRY

--- kodilla-intro/src/main/java/com/kodilla/FirstClass.java
+++ kodilla-intro/src/main/java/com/kodilla/FirstClass.java
@@ -4,31 +4,20 @@ public class FirstClass {
     public static void main(String[] args) {
 
         Notebook notebook = new Notebook(600, 8000, 2025);
-        System.out.println("notebook.weight: " + notebook.weight +
-                "" + ", notebook.price:" + notebook.price +
-                "" + ", year:" + notebook.year);
-        notebook.checkPrice();
-        notebook.checkWeight();
-        notebook.assessNotebookCondition();
-
-
         Notebook heavyNotebook = new Notebook(2000, 3000, 2020);
-        System.out.println("heavyNotebook:" + heavyNotebook.weight +
-                "" + ", heavyNotebook.price:" + heavyNotebook.price +
-                "" + ", year:" + heavyNotebook.year);
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.assessNotebookCondition();
-
-
         Notebook oldNotebook = new Notebook(1600, 1000, 2019);
-        System.out.println("oldNotebook:" + oldNotebook.weight +
-                "" + ", oldNotebook.price:" + oldNotebook.price +
-                "" + ", year:" + oldNotebook.year);
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.assessNotebookCondition();
 
+        displayNotebookInfo(notebook, "notebook");
+        displayNotebookInfo(heavyNotebook, "heavyNotebook");
+        displayNotebookInfo(oldNotebook, "oldNotebook");
+    }
+
+    public static void displayNotebookInfo(Notebook notebook, String label) {
+        System.out.println(label + ":weight: " + notebook.weight +
+                ", price: " + notebook.price + ", year:" + notebook.year);
+        notebook.checkWeight();
+        notebook.checkPrice();
+        notebook.assessNotebookCondition();
 
     }
 }
