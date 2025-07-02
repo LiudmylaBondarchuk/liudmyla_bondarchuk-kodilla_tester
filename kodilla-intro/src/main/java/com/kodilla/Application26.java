c69e79273106e5e003d117543f1f3fb413a8c7d6
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-07-02 16:28:01 +0200

    Refactor: convert procedural code to object-oriented structure

---
+++ kodilla-intro/src/main/java/com/kodilla/Application26.java
@@ -0,0 +1,23 @@
+/* public class Application {
+   public static void main(String[] args) {
+      String name = "Adam";
+      double age = 40.5;
+      double height = 178;
+
+      if (name != null) {
+         if (age > 30 && height > 160) {
+            System.out.println("User is older than 30 and taller than 160cm");
+         } else {
+            System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+         }
+      }
+  }
+}*/
+package com.kodilla;
+    public class Application26 {
+        public static void main(String[] args) {
+            UserStatisticsApp user1 = new UserStatisticsApp("Adam", 40.5, 178);
+            user1.checkNameAgeWeight();
+        }
+
+    }
---
+++ kodilla-intro/src/main/java/com/kodilla/UserStatisticsApp.java
@@ -0,0 +1,30 @@
+package com.kodilla;
+
+public class UserStatisticsApp {
+    String name;
+    double age;
+    double height;
+
+    public UserStatisticsApp(String name, double age,double height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+    public String getName (){
+        return name;
+    }
+    public  double getAge(){
+        return age;
+    }
+    public double getHeight(){
+        return height;
+    }
+
+    public void checkNameAgeWeight(){
+        if (age > 30 && height > 160) {
+                System.out.println("User is older than 30 and taller than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
