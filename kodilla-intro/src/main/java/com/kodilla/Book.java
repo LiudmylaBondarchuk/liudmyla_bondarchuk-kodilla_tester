72b76c360206a281706ca77de12910371cc383e1
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-07-05 11:17:20 +0200

    feat: add Book class with static of() method

---
+++ kodilla-intro/src/main/java/com/kodilla/Book.java
@@ -0,0 +1,23 @@
+package com.kodilla;
+
+public class Book {
+    private String author;
+    private String title;
+
+    private Book (String author, String title){
+        this.author = author;
+        this.title = title;
+    }
+
+    public static  Book of (String author,String title){
+        return new Book(author, title);
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+}
---
+++ kodilla-intro/src/main/java/com/kodilla/UserDialogs.java
@@ -0,0 +1,7 @@
+package com.kodilla;
+import java.util.Scanner;
+public class UserDialogs {
+    public String getUserName(){
+        Scanner scanner =new Scanner( )
+    }
+}
