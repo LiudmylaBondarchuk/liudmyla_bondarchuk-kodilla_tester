3cf5e61f87fb0579e1d140bc7f29ed666eef4a61
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-07-07 16:15:16 +0200

    Implement course enum with switch-based course description output

---
+++ kodilla-intro/src/main/java/com/kodilla/MyCourseClass.java
@@ -0,0 +1,18 @@
+package com.kodilla;
+enum KodillaCourse {
+    JD,  /* Java Developer */
+    JDP, /* Java Developer Plus */
+    TA,  /* Tester Automatyzujący */
+    TAI  /* Tester Automatyzujący z egzaminem ISTQB */
+}
+public class MyCourseClass {
+    public static void main(String[] args) {
+        KodillaCourse myCourse = KodillaCourse.TA;
+        switch (myCourse) {
+            case JD -> System.out.println("Java Developer");
+            case JDP -> System.out.println("Java Developer Plus");
+            case TA -> System.out.println("Tester Automat.");
+            case TAI -> System.out.println("Tester Automat. z ISTQB");
+        }
+    }
+}
