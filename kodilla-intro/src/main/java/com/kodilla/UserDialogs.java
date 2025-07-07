5ea3c731bb0c40eedbeac27d819bc72258429293
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-07-07 16:25:13 +0200

    feat: add user color selection via getColor()

--- kodilla-intro/src/main/java/com/kodilla/UserDialogs.java
+++ kodilla-intro/src/main/java/com/kodilla/UserDialogs.java
@@ -19,10 +19,14 @@ public class UserDialogs {
             System.out.println("Select calculation (A-add, S-subtract, D-divide, M-multiply):");
             String calc = scanner.nextLine().trim().toUpperCase();
             switch (calc) {
-                case "A": return "ADD";
-                case "S": return "SUB";
-                case "D": return "DIV";
-                case "M": return "MUL";
+                case "A":
+                    return "ADD";
+                case "S":
+                    return "SUB";
+                case "D":
+                    return "DIV";
+                case "M":
+                    return "MUL";
                 default:
                     System.out.println("Wrong calculation. Try again.");
             }
@@ -36,55 +40,30 @@ public class UserDialogs {
         return val;
     }
 
-    public static Color getColor(){
+    public static Color getColor() {
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        while (true) {
             System.out.println("Select color: B-BLACK, W-WHITE, " +
                     "P-PINK, G-GREEN, O-ORANGE, Y-YELLOW, BL- BLUE:");
             String input = scanner.nextLine().trim().toUpperCase();
-            switch (input){
-                case "B": return Color.BLACK;
-                case "W": return Color.WHITE;
-                case "P": return Color.PINK;
-                case "G": return Color.GREEN;
-                case "O": return Color.ORANGE;
-                case "Y": return Color.YELLOW;
-                case "BL": return Color.BLUE;
+            switch (input) {
+                case "B":
+                    return Color.BLACK;
+                case "W":
+                    return Color.WHITE;
+                case "P":
+                    return Color.PINK;
+                case "G":
+                    return Color.GREEN;
+                case "O":
+                    return Color.ORANGE;
+                case "Y":
+                    return Color.YELLOW;
+                case "BL":
+                    return Color.BLUE;
                 default:
                     System.out.println("Invalid selection. Try again.");
             }
         }
     }
 }
-/*import java.util.Scanner;
-public class UserDialogs {
-   public static String getUsername() {
-      Scanner scanner = new Scanner(System.in);
-      while (true) {
-         System.out.println("Enter your name: ");
-         String name = scanner.nextLine().trim();
-         if (name.length() >= 2) {
-            return name;
-         }
-         System.out.println("Name is too short. Try again.");
-      }
-   }
-
-   public static String getUserSelection() {
-      Scanner scanner = new Scanner(System.in);
-      while (true) {
-         System.out.println("Select calculation (A-add, S-subtract, D-divide, M-multiply):");
-         String calc = scanner.nextLine().trim().toUpperCase();
-         if (calc.equals("A")) {
-            return "ADD";
-         } else if (calc.equals("S")) {
-            return "SUB";
-         } else if (calc.equals("D")) {
-            return "DIV";
-         } else if (calc.equals("M")) {
-            return "MUL";
-         }
-         System.out.println("Wrong calculation. Try again.");
-      }
-   }
-}*/
