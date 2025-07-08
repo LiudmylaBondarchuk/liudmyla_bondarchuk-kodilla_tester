981beb08f74be32a23652b20fae4e987e4d98dca
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-07-08 22:36:36 +0200

    Refactor:Implement Color enum and input selection in a single class

--- kodilla-intro/src/main/java/com/kodilla/ColorApplication.java
+++ kodilla-intro/src/main/java/com/kodilla/ColorApplication.java
@@ -1,6 +1,5 @@
-package com.kodilla;
-
-enum Color {
+import java.util.Scanner;
+ enum Color {
     BLACK,
     WHITE,
     PINK,
@@ -10,10 +9,33 @@ enum Color {
     YELLOW,
     BLUE
 }
-public class ColorApplication {
-    public static void main(String[] args) {
-        Color myColor = UserDialogs.getColor();
-            System.out.println("Your color:" + myColor);
 
+public class ColorApplication {
+
+    public static Color getColor() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Select color: B-BLACK, W-WHITE, " +
+                    "P-PINK, G-GREEN, R-RED, " +
+                    "O-ORANGE, Y-YELLOW, BL-BLUE:");
+            String input = scanner.nextLine().trim().toUpperCase();
+            switch (input) {
+                case "B": return Color.BLACK;
+                case "W": return Color.WHITE;
+                case "P": return Color.PINK;
+                case "G": return Color.GREEN;
+                case "R": return Color.RED;
+                case "O": return Color.ORANGE;
+                case "Y": return Color.YELLOW;
+                case "BL": return Color.BLUE;
+                default:
+                    System.out.println("Invalid selection. Try again.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Color myColor = getColor();
+        System.out.println("Your color: " + myColor);
     }
 }
