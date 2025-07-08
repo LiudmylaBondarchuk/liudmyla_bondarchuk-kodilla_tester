65d25980c23d119c71124b52607f028a67d1601a
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-07-08 22:41:17 +0200

    Refactor: Remove unused code from UserDialogs

--- kodilla-intro/src/main/java/com/kodilla/UserDialogs.java
+++ kodilla-intro/src/main/java/com/kodilla/UserDialogs.java
@@ -40,30 +40,4 @@ public class UserDialogs {
         return val;
     }
 
-    public static Color getColor() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Select color: B-BLACK, W-WHITE, " +
-                    "P-PINK, G-GREEN, O-ORANGE, Y-YELLOW, BL- BLUE:");
-            String input = scanner.nextLine().trim().toUpperCase();
-            switch (input) {
-                case "B":
-                    return Color.BLACK;
-                case "W":
-                    return Color.WHITE;
-                case "P":
-                    return Color.PINK;
-                case "G":
-                    return Color.GREEN;
-                case "O":
-                    return Color.ORANGE;
-                case "Y":
-                    return Color.YELLOW;
-                case "BL":
-                    return Color.BLUE;
-                default:
-                    System.out.println("Invalid selection. Try again.");
-            }
-        }
-    }
 }
