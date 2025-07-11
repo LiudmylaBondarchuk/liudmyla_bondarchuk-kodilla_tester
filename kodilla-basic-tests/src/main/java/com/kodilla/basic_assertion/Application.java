2e8ce3dfd16aa41d6d6246b74d2899b3ed7a393c
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-07-11 20:36:35 +0200

    Implement Calculator methods and tests: sum, subtract, square

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,31 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        boolean isSumCorrect = ResultChecker.assertEquals(13, sumResult);
+        if (isSumCorrect) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+        int subtractResult = calculator.subtract(a, b);
+        boolean isSubbtractCorrect = ResultChecker.assertEquals(-3, subtractResult);
+        if (isSubbtractCorrect) {
+            System.out.println("Metoda subbtract działa poprawnie dla liczb "+ a + " i " + b);
+        }else {
+            System.out.println("Metoda subbtrackt nie działa poprawnie dla liczb " + a + "i " + b);
+        }
+
+        int squareResult = calculator.square(a);
+        boolean isSquareCorrect = ResultChecker.assertEquals(25, squareResult);
+        if (isSquareCorrect){
+            System.out.println("Metoda square działa poprawnie dla liczby " + a);
+        }else{
+            System.out.println("Metoda square nie działa poprawnie dla liczby " + a);
+        }
+    }
+}
