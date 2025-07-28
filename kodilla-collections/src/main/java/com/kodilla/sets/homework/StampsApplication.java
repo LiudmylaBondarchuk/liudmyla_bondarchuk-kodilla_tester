aa931421f3c7938f7525a9e0cb7eb25ec3456f19
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-07-28 19:45:06 +0200

    Add Stamp class and StampsApplication using Set

---
+++ kodilla-collections/src/main/java/com/kodilla/sets/homework/StampsApplication.java
@@ -0,0 +1,17 @@
+package com.kodilla.sets.homework;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StampsApplication {
+    public static void main(String[] args) {
+        Set<Stamp> stamps = new HashSet<>();
+        stamps.add(new Stamp("Warsaw 2021", 2.0, 3.0, true));
+        stamps.add(new Stamp("Gdańsk 2019", 1.5, 2.5, false));
+        stamps.add(new Stamp("London 1999", 2.0, 3.0, true));
+        stamps.add(new Stamp("Warsaw 2021", 2.0, 3.0, true));
+        System.out.println(stamps.size());
+        for (Stamp stamp : stamps) {
+            System.out.println(stamp);
+        }
+    }
+}
