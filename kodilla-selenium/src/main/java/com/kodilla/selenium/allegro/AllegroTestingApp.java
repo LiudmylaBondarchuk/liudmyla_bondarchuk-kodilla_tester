317922539b1ea59632fdd3f254bccc0df863e6b5
Liudmyla Bondarchuk <liudmyla.bondarchuk.lb@gmail.com>
2025-08-28 13:13:35 +0200

    Refactor XPath selectors to CSS, add wait for first search result, and log product info

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -9,21 +9,26 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class AllegroTestingApp {
     public static void main(String[] args) {
+
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\Chrome\\chromedriver.exe");
+
         ChromeOptions options = new ChromeOptions();
         WebDriver driver = new ChromeDriver(options);
 
-        driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-
         try {
+            driver.manage().window().maximize();
+
+            WebDriverWait wait = new WebDriverWait(driver, 20);
+
             driver.get("https://allegro.pl/");
 
             try {
                 WebElement acceptCookies = wait.until(
-                        ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-role='accept-consent']"))
+                        ExpectedConditions.elementToBeClickable(By.cssSelector("button[data-role='accept-consent']"))
                 );
                 acceptCookies.click();
                 System.out.println("Accepted cookies.");
@@ -32,26 +37,37 @@ public class AllegroTestingApp {
             }
 
             WebElement categoryDropdown = wait.until(
-                    ExpectedConditions.presenceOfElementLocated(By.xpath("//select[@data-role='search-scope-select']"))
+                    ExpectedConditions.presenceOfElementLocated(By.cssSelector("select[data-role='search-scope-select']"))
             );
             Select selectCategory = new Select(categoryDropdown);
             selectCategory.selectByVisibleText("Elektronika");
             System.out.println("Selected category 'Electronics'.");
 
             WebElement searchBox = wait.until(
-                    ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@data-role='search-input']"))
+                    ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[data-role='search-input']"))
             );
             searchBox.clear();
             searchBox.sendKeys("Mavic mini");
             System.out.println("Entered 'Mavic mini'.");
 
             WebElement searchButton = wait.until(
-                    ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']//span[text()='szukaj']"))
+                    ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit'] span"))
             );
             searchButton.click();
             System.out.println("Clicked 'Search'.");
 
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("section > div > article")));
+
+            List<WebElement> products = driver.findElements(By.cssSelector("section > div > article"));
+
+            System.out.println("Found " + products.size() + " products:");
+            for (WebElement product : products) {
+                System.out.println(product.getText());
+                System.out.println("--------------------");
+            }
+
         } finally {
+            driver.quit();
         }
     }
 }
